e865d4e7564dc1bbe1d3b0f96ed872fdc7468853
ElishaGiles <elisha.g.giles@gmail.com>
2016-08-31 13:30:53 -0600

    completed project

--- controller.js
+++ controller.js
@@ -1,10 +1,29 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+$scope.pageNum = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    var promise = mainService.getUsers($scope.pageNum);
+
+    promise.then(function(response) {
+      $scope.users = response.data.data;
+      $scope.totalPages = response.data.total_pages;
+    });
+  }
+  $scope.getUsers();
+
+  $scope.previous = function() {
+    if($scope.pageNum > 1) {
+      $scope.pageNum--;
+      $scope.getUsers();
+    }
   }
 
-
-  $scope.getUsers();
+  $scope.next = function() {
+    if($scope.totalPages > $scope.pageNum) {
+      $scope.pageNum++;
+      $scope.getUsers();
+    }
+  }
 
 });
